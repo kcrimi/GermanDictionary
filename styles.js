0b5a4d20e91eaba377e0884747299a16d8d9bbdf
kcrimi <kevincrimi@gmail.com>
2016-03-31 00:10:01 -0400

    german dictionary working

---
+++ styles.js
@@ -0,0 +1,23 @@
+import React, {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+	parent: {
+		padding: 20,
+	},
+	textInput: {
+		marginTop:20,
+		height:30,
+		backgroundColor: '#F5FCFF',
+	},
+	germanLabel: {
+		marginTop:20,
+		fontWeight: 'bold',
+	},
+	germanWord: {
+		marginTop:15,
+		fontSize:30,
+		fontStyle: 'italic',
+	}
+});
+
+module.exports = styles; 
